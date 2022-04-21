b2ebc2f353fca5905ddbe74f530678727527e8c4
vatsal <vatsalpatani@gmail.com>
2022-04-21 13:58:22 +0530

    add

---
+++ 33.dart
@@ -0,0 +1,18 @@
+class Hello {
+  parin() {
+    print('This is a Parin Solanki\tThis is a Parin Solanki');
+  }
+  // printing();
+}
+
+class Hello1 extends Hello{
+  Hello1(){
+  print("This is Class 2");
+   parin();
+ }
+}
+void main()
+{
+  var obj = new Hello1();
+}
+
